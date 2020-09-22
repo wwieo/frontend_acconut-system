15e6d0764fa49ed2206a7c89313677ed84db7219
LeoWang <wwieo055169@gmail.com>
2020-09-22 22:51:30 +0800

    Finish frontend user_action validation

--- src/layout/login.js
+++ src/layout/login.js
@@ -4,18 +4,75 @@ import { MDBBtn } from 'mdbreact';
 import '../style/user_action.css';
 
 class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            status:{
+                account: "",
+                password: ""
+            },
+            error:{
+                accountAlert: false,
+                passwordAlert: false
+            }
+        }
+        this.handleBlur = this.handleBlur.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(e){
+        let status = this.state;
+        const fieldName = e.target.name;
+        const fieldValue = e.target.value;  
+
+        status[fieldName] = fieldValue;
+        this.setState({status:status});
+    }
+    handleBlur(e) {
+        let status = this.state;
+        const fieldName = e.target.name;
+        const fieldValue = e.target.value; 
+        switch(fieldName){
+            case 'account':
+                if (fieldValue.length < 4)
+                    status["accountAlert"] = true;
+                else
+                    status["accountAlert"] = false;
+                this.setState({status:status});
+                break;
+            case 'password':
+                if (fieldValue.length < 6)
+                    status["passwordAlert"] = true;
+                else
+                    status["passwordAlert"] = false;
+                this.setState({status:status});
+                break;
+            default :
+                break;
+
+        }
+    } 
     render() {
         return (
             <div>
                 <br/>
                 <div className="form-group">
                     <label>Username or email</label>
-                    <input type="text" className="form-control" placeholder="Enter user name or email" />
+                    <label className="alert">{this.state.accountAlert? 
+                                              "* Length should be upper than 3":""}</label>
+                    <input type="text" onBlur={this.handleBlur} onChange={this.handleChange}
+                           value={this.state.account} name="account"
+                           className="form-control" placeholder="Enter user name or email"  
+                           style={{borderColor:this.state.accountAlert?"red":""}}/>
                 </div><br/>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <label className="alert">{this.state.passwordAlert? 
+                                              "* Length should be upper than 5":""}</label>
+                    <input type="password" onBlur={this.handleBlur} onChange={this.handleChange}
+                           value={this.state.password} name="password"
+                           className="form-control" placeholder="Enter password"  
+                           style={{borderColor:this.state.passwordAlert?"red":""}}/>
                 </div><br/>
 
                 <div className="text-center">
--- src/layout/sign_up.js
+++ src/layout/sign_up.js
@@ -8,18 +8,18 @@ class SignUp extends React.Component {
         super(props);
         this.state={
             status:{
-                userNameValue: "",
-                fullNameValue: "",
-                emailValue: "",
-                passwordValue: "",
-                cfPasswordValue: ""
+                userName: "",
+                fullName: "",
+                email: "",
+                password: "",
+                cfPassword: ""
             },
-            alertText:{
-                userNameAlert: "",
-                fullNameAlert: "",
-                emailAlert: "",
-                passwordAlert: "",
-                cfPasswordAlert: ""
+            error:{
+                userNameAlert: false,
+                fullNameAlert: false,
+                emailAlert: false,
+                passwordAlert: false,
+                cfPasswordAlert: false
             }
         }
         this.handleBlur = this.handleBlur.bind(this);
@@ -38,35 +38,44 @@ class SignUp extends React.Component {
         const fieldName = e.target.name;
         const fieldValue = e.target.value; 
         switch(fieldName){
-            case 'userNameValue':
+            case 'userName':
                 if (fieldValue.length > 24 || fieldValue.length < 4)
-                    status["userNameAlert"] = "* Length should be upper than 3 and lower than 25";
+                    status["userNameAlert"] = true;
                 else
-                    status["userNameAlert"] = "";
+                    status["userNameAlert"] = false;
                 this.setState({status:status});
                 break;
-            case 'fullNameValue':
+            case 'fullName':
                 if (fieldValue.length > 24 || fieldValue.length < 1)
-                    status["fullNameAlert"] = "* Length should be upper than 0 and lower than 25";
+                    status["fullNameAlert"] = true; 
                 else
-                    status["fullNameAlert"] = "";
+                    status["fullNameAlert"] = false;
                 this.setState({status:status});
                 break;
-            case 'emailValue':
-
+            case 'email':
+                if (/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(fieldValue))
+                    status["emailAlert"] = false;
+                else
+                    status["emailAlert"] = true; 
                 this.setState({status:status});
                 break;
-            case 'passwordValue':
+            case 'password':
                 if (fieldValue.length < 6)
-                    status["passwordAlert"] = "* Length should be upper than 5";
+                    status["passwordAlert"] = true;
                 else
-                    status["passwordAlert"] = "";
+                    status["passwordAlert"] = false;
                 this.setState({status:status});
                 break;
-            case 'cfPasswordValue':
-                
-                this.setState({status:status});    
-                break;    
+            case 'cfPassword':
+                if (fieldValue !== status["passwordValue"])
+                    status["cfPasswordAlert"] = true;
+                else
+                    status["cfPasswordAlert"] = false;
+                this.setState({status:status});  
+                break;
+            default :
+                break;
+
         }
     }   
 
@@ -77,42 +86,52 @@ class SignUp extends React.Component {
                 <br/>
                 <div className="form-group">
                     <label>User Name</label>                    
-                    <label className="alert">{this.state.userNameAlert}</label>
+                    <label className="alert">{this.state.userNameAlert? 
+                                              "* Length should be upper than 3 and lower than 25":""}</label>
                     <input type="text" onBlur={this.handleBlur} onChange={this.handleChange}
-                           value={this.state.userNameValue} name="userNameValue"
-                           className="form-control" placeholder="Enter user name" />
+                           value={this.state.userName} name="userName"
+                           className="form-control" placeholder="Enter user name" 
+                           style={{borderColor:this.state.userNameAlert?"red":""}}/>
                 </div><br/>
 
                 <div className="form-group">
                     <label>Full Name</label>
-                    <label className="alert">{this.state.fullNameAlert}</label>
+                    <label className="alert">{this.state.fullNameAlert?
+                                              "* Length should be upper than 0 and lower than 25":""}</label>
                     <input type="text" onBlur={this.handleBlur} onChange={this.handleChange}
-                           value={this.state.fullNameValue} name="fullNameValue"
-                           className="form-control" placeholder="Enter full name" />
+                           value={this.state.fullName} name="fullName"
+                           className="form-control" placeholder="Enter full name" 
+                           style={{borderColor:this.state.fullNameAlert?"red":""}}/>
                 </div><br/>
 
                 <div className="form-group">
                     <label>Email address</label>
-                    <label className="alert">{this.state.emailAlert}</label>
+                    <label className="alert">{this.state.emailAlert?
+                                              "* Email pattern is not correct":""}</label>
                     <input type="email"  onBlur={this.handleBlur} onChange={this.handleChange}
-                           value={this.state.emailValue} name="emailValue"
-                           className="form-control" placeholder="Enter email" />
+                           value={this.state.email} name="email"
+                           className="form-control" placeholder="Enter email"  
+                           style={{borderColor:this.state.emailAlert?"red":""}}/>
                 </div><br/>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <label className="alert">{this.state.passwordAlert}</label>
+                    <label className="alert">{this.state.passwordAlert?
+                                              "* Length should be upper than 5":""}</label>
                     <input type="password" onBlur={this.handleBlur} onChange={this.handleChange}
-                           value={this.state.passwordValue} name="passwordValue"
-                           className="form-control" placeholder="Enter password" />
+                           value={this.state.password} name="password"
+                           className="form-control" placeholder="Enter password" 
+                           style={{borderColor:this.state.passwordAlert?"red":""}}/>
                 </div><br/>
 
                 <div className="form-group">
                     <label>Password confirmed</label>
-                    <label className="alert">{this.state.cfPasswordAlert}</label>
+                    <label className="alert">{this.state.cfPasswordAlert?
+                                              "* Not the same password":""}</label>
                     <input type="password"  onBlur={this.handleBlur} onChange={this.handleChange}
-                           value={this.state.cfPasswordValue} name="cfPasswordValue"
-                           className="form-control" placeholder="Enter password again" />
+                           value={this.state.cfPassword} name="cfPassword"
+                           className="form-control" placeholder="Enter password again" 
+                           style={{borderColor:this.state.cfPasswordAlert?"red":""}}/>
                 </div><br/>
                 
 
